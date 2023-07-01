0aecdc2f98aa91d88182b58d6629058aa0bee9a8
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-01 13:44:18 +0300

    :white_check_mark:Add test for visit method

--- spec/association_spec.rb
+++ spec/association_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../association'
 require_relative '../classes'
+require 'date'
 
 describe Owner do
     let (:name) {'John'}
@@ -39,5 +40,27 @@ describe Vet do
        it 'sets empty visits array' do
         expect(vet.visits).to be_empty
        end
+    end
+end
+
+describe Visit do
+    let(:date) {Date.new(2023, 7, 1)}
+    let(:animal) {double("Animal", visits: [])}
+    let(:vet) {double("Vet", visits: [])}
+
+    subject{Visit.new(date, animal, vet)}
+
+    context 'initialize' do
+        it 'sets date' do
+            expect(subject.date).to eq(date)
+        end
+
+        it 'add to animal visits' do
+            expect(animal.visits).to include(subject)
+        end
+
+        it 'add to vet visits' do
+            expect(vet.visits).to include(subject)
+        end
     end
 end
