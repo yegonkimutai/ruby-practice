f35d5b0db78c5733bc831f43aa3200cd5979c28a
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-16 16:20:12 +0300

    :zap:Update tests for animals class

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -1,5 +1,40 @@
 require_relative '../classes'
 
+describe Animal do
+    let(:type) { 'Dog' }
+    let(:number_of_legs) { 4 }
+    let(:name) { 'Tom' }
+    let(:animal) { Animal.new(type, number_of_legs, name) }
+
+    context 'initialize' do
+        it 'sets type' do
+            expect(animal.type).to eq(type)
+        end
+
+        it 'sets name' do
+            expect(animal.name).to eq(name)
+        end
+
+        it 'sets type' do
+            expect(animal.number_of_legs).to eq(number_of_legs)
+        end
+    end
+
+    context 'methods' do
+        it 'makes animal speak' do
+            expect(animal.speak).to eq('grrrr')
+        end
+
+        it 'removes leg' do
+            expect(animal.remove_leg).to eq(3)
+        end
+
+        it 'removes leg' do
+            expect(animal.likes_food?('Bamboo')).to be(false)
+        end
+    end
+end
+
 describe Dog do
     let (:name) {'Bob'}
     let (:color) {'brown'}
